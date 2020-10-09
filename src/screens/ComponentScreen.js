8515dc8f9c9315c2b2e35d53554b78f462d36942
codingwithak <kristianportillo@Kristians-MacBook-Air.local>
2020-10-08 21:02:49 -0400

    first commit

---
+++ src/screens/ComponentScreen.js
@@ -0,0 +1,18 @@
+import React,{useEffect} from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+
+const ComponentsScreen = () =>{
+
+
+
+
+    return(
+        <View>
+        
+        </View>
+
+    )
+}
+export default ComponentsScreen
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,44 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
+
+
+const handleClick = () => {
+    // props.navigation.navigate("Components")
+    fetch("http://localhost:3000/games", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({})})
+ 
+
+    
+}
+const HomeScreen = (props) => {
+
+    
+    return(  
+        <View style={styles.container}>
+            <Image source={{
+            uri:
+            "https://www.clipartkey.com/mpngs/m/220-2207505_bored-boring-lame-freetoedit.png"
+            }}style={{ width: 300, height: 100}}/>
+            <Text>  Games </Text>
+            <Button 
+                onPress={()=> handleClick()}
+                title="Start Game"
+            />         
+            <StatusBar style="auto" />
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+export default HomeScreen;
